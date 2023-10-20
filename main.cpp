b501586f5eb49fe87e4f12988e4319fd98f515c7
florian <florianmarquier1@gmail.com>
2023-10-20 14:17:45 +0200

    interface

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int valeur;
+
+    cout<< "Bienvenue au CTA/CODIS, dans la salle de regulation ou nous recevons tous les appel du 18, 15 et 112"<<endl;
+    cout<< "Votre mission est de repondre au appel et de declencher les secours nessecaire, de gerer les interventions en cours, et de cloturer les intervention terminer"<<endl;
+    cout<< "1) Saisir  un appel"<<endl;
+    cout<< "2) Afficher les interventions"<<endl;
+    cout<< "3) Cloture une intervention"<<endl;
+    cin>> valeur;
+
+    if (valeur == 1)
+    {
+        
+    }
+
+
+
+
+    return 0;
+}
